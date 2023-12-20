f4e0aa66f9d322a55586c1f05348a7986739a370
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-20 17:46:02 -0500

    Added a mini loadData seeder for testing purposes.

--- inventory-service/src/main/java/com/ymsaadi/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ymsaadi/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.ymsaadi.inventoryservice;
 
+import com.ymsaadi.inventoryservice.model.Inventory;
+import com.ymsaadi.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13_red");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
 }
