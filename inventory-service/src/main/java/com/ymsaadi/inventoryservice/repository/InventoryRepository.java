13157b3269c040728e7ea28f729215920eda145f
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-21 19:09:29 -0500

    Refactored isInStock method to take multiple skuCodes and returns a List of InventoryResponse objects instead of a boolean.

--- inventory-service/src/main/java/com/ymsaadi/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/ymsaadi/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,8 @@ package com.ymsaadi.inventoryservice.repository;
 import com.ymsaadi.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- inventory-service/src/main/java/com/ymsaadi/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/ymsaadi/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.ymsaadi.inventoryservice.service;
 
+import com.ymsaadi.inventoryservice.dto.InventoryResponse;
 import com.ymsaadi.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +14,15 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository
+                .findBySkuCodeIn(skuCode)
+                .stream()
+                .map(inventory -> InventoryResponse
+                            .builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build())
+                .toList();
     }
 }
