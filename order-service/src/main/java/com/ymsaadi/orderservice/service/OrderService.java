5346766491335fccd7af5f152c4f40024c959771
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-26 17:40:54 -0500

    Added Kafka and set up the order service as the producer.

--- order-service/src/main/java/com/ymsaadi/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ymsaadi/orderservice/service/OrderService.java
@@ -3,12 +3,14 @@ package com.ymsaadi.orderservice.service;
 import com.ymsaadi.orderservice.dto.InventoryResponse;
 import com.ymsaadi.orderservice.dto.OrderLineItemDto;
 import com.ymsaadi.orderservice.dto.OrderRequest;
+import com.ymsaadi.orderservice.event.OrderPlacedEvent;
 import com.ymsaadi.orderservice.model.Order;
 import com.ymsaadi.orderservice.model.OrderLineItem;
 import com.ymsaadi.orderservice.repository.OrderRepository;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -24,6 +26,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -54,6 +57,7 @@ public class OrderService {
 
             if (allProductsInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order place successfully!";
             } else {
                 throw new IllegalArgumentException("A product is not in stock, please try again later.");
