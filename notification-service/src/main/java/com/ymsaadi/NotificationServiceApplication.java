63a2a2f212866803bff54c89c071213a081726e4
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-26 17:42:12 -0500

    Implemented the notification service and set it up as the consumer.

---
+++ notification-service/src/main/java/com/ymsaadi/NotificationServiceApplication.java
@@ -0,0 +1,20 @@
+package com.ymsaadi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        // todo send email
+        log.info("received notification for order - {}", orderPlacedEvent.getOrderNumber());
+    }
+}
