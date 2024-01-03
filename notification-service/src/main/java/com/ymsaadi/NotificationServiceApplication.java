903c24bd2b937e3dfc939748401feada9b27ac5d
ymsaadi <youssefmsaadi95@gmail.com>
2024-01-02 19:00:39 -0500

    Added email support in the notification service.

--- notification-service/src/main/java/com/ymsaadi/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/ymsaadi/NotificationServiceApplication.java
@@ -1,13 +1,18 @@
 package com.ymsaadi;
 
+import com.ymsaadi.service.NotificationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 @Slf4j
 public class NotificationServiceApplication {
+    private final NotificationService notificationService;
+
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
@@ -15,6 +20,8 @@ public class NotificationServiceApplication {
     @KafkaListener(topics = "notificationTopic")
     public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
         // todo send email
+        notificationService.sendEmail("test@test.com",
+                "Order placed!", "received notification for order - " + orderPlacedEvent.getOrderNumber());
         log.info("received notification for order - {}", orderPlacedEvent.getOrderNumber());
     }
 }
---
+++ notification-service/src/main/java/com/ymsaadi/service/NotificationService.java
@@ -0,0 +1,20 @@
+package com.ymsaadi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        javaMailSender.send(message);
+    }
+}
